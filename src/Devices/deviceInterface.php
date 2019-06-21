1dbfeee870ab14f266690b6bd6fd28540f805aa4
Gregory Bers <gregorybers@epiecs.be>
2019-06-21 12:09:02 +0200

    updated docblock in interface

--- src/Devices/deviceInterface.php
+++ src/Devices/deviceInterface.php
@@ -8,16 +8,8 @@ interface deviceInterface
 	 * Runs one or more cli commands on a device.
 	 * Eg. the standard linux cli in junos or user exec mode in cisco ios
 	 *
-	 * Sends commands one by one and does not chain commands. So after every command you return back to your initial state.
-	 * For example run cd /tmp and then pwd you will still get back your standard home directory. If you need to chain
-	 * commands you need to chain them with ; or &&
-	 *
-	 * cd /tmp ; pwd runs both commands back to back and ignores the return code of previous commands
-	 *
-	 * cd /tmp && pwd only runs pwd if the previous command (cd /tmp) returns status 0 (command successfull)
-	 *
-	 * This way the user still has enough flexibility. It would have been easy to just implode all the commands with ;
-	 * or && but that would make this library to opiniated imho.
+     * Commands can be chained and executed sequentially. However this is only per set of supplied commands.
+	 * After each block of commands the configuration mode of the device will be exited.
 	 *
 	 * @param mixed $commands Either a string containing one command or an array containing multiple command's
 	 * @return array Returns an array with the output of each command with the command as key
@@ -29,7 +21,8 @@ interface deviceInterface
 	 * Runs one or more operational/enable commands on a device.
 	 * Eg. cli mode in junos or privileged exec mode in cisco ios
 	 *
-	 * Sends commands one by one and does not chain commands. So after every command you return back to your initial state.
+     * Commands can be chained and executed sequentially. However this is only per set of supplied commands.
+	 * After each block of commands the configuration mode of the device will be exited.
 	 *
 	 * @param mixed $commands Either a string containing one command or an array containing multiple command's
 	 * @return array Returns an array with the output of each command with the command as key
@@ -41,7 +34,7 @@ interface deviceInterface
 	 * Sends one or more configuration commands to the device.
 	 * Eg. configuration/edit mode in junos or global configuration in cisco ios
 	 *
-	 * In this mode commands can be chained and executed sequentially. However this is only per set of supplied commands.
+	 * Commands can be chained and executed sequentially. However this is only per set of supplied commands.
 	 * After each block of commands the configuration mode of the device will be exited.
 	 *
 	 * @param mixed $commands Either a string containing one command or an array containing multiple command's
