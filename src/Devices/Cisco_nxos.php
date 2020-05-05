ba1aba9686c86a4f5175d7ed7830706aa644d98f
Gregory Bers <gregorybers@epiecs.be>
2020-05-05 11:50:49 +0200

    Added support for Cisco NX-OS devices

---
+++ src/Devices/Cisco_nxos.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace Epiecs\PhpMiko\Devices;
+
+class Cisco_nxos implements DeviceInterface
+{
+	/**
+	 * Holds the ssh connection
+	 * @var object
+	 */
+
+	public $conn;
+
+	/**
+	 * Contains the secret password if one is needed. Eg. Enable mode on a cisco device.
+	 * @var boolean
+	 */
+
+	public $secret = '';
+
+    /**
+     * Patterns used to read the shell
+     */
+
+    private $shellPattern              = '/.*#.*$/m';
+    private $privilegedExecModePattern = '/.*#.*$/m';
+    private $configurationModePattern  = '/.*\([\w-]+\)#.*$/m';
+
+    /**
+	 * Constructor. Expects a ssh2 object
+	 * @param object $connection SSH2 object
+	 */
+
+	public function __construct($connection)
+	{
+		$this->conn = $connection;
+	}
+
+	/**
+	 * Sends one or more cli command(s)
+	 * @param  mixed $commands String with one command or array containing multiple commands
+	 * @return string           Returns all output
+	 */
+
+    public function cli($commands) : array
+    {
+		$output = array();
+
+        // First we do a basic cleanup of the shell
+        $this->conn->read($this->shellPattern, $this->conn::READ_REGEX);
+
+        $this->conn->write("terminal length 0\n");
+
+        // Clean up the shell
+        $this->conn->read($this->privilegedExecModePattern, $this->conn::READ_REGEX);
+
+		foreach($commands as $command)
+		{
+            $this->conn->write("{$command}\n");
+            $output[$command] = $this->conn->read($this->shellPattern, $this->conn::READ_REGEX);
+        }
+
+		$this->conn->write("terminal no length\n");
+        $this->conn->read($this->shellPattern, $this->conn::READ_REGEX);
+
+        return $output;
+    }
+
+	/**
+	 * Sends one or more  operation command(s)
+	 * @param  mixed $commands String with one command or array containing multiple commands
+	 * @return string           Returns all output
+	 */
+
+    public function operation($commands) : array
+    {
+        //NX-os does not use privileged exec mode. We can just use cli commands here
+		return $this->cli($commands);
+    }
+
+	/**
+	 * Sends one or more  configuration command(s)
+	 * @param  mixed $commands String with one command or array containing multiple commands
+	 * @return string           Returns all output
+	 */
+
+    public function configure($commands) : array
+    {
+        $output = array();
+
+        // First we do a basic cleanup of the shell
+		$this->conn->read($this->shellPattern, $this->conn::READ_REGEX);
+
+        $this->conn->write("terminal length 0\n");
+
+        // Clean up the shell
+        $this->conn->read($this->privilegedExecModePattern, $this->conn::READ_REGEX);
+
+        $this->conn->write("configure terminal\n");
+        $this->conn->read($this->configurationModePattern, $this->conn::READ_REGEX); //Clear with full read
+
+		// Loop commands
+		foreach($commands as $command)
+		{
+			$this->conn->write("{$command}\n");
+
+			// Read the data and add it to the output
+			$output[$command] = $this->conn->read($this->configurationModePattern, $this->conn::READ_REGEX);
+		}
+
+		// Exit the cli
+		$this->conn->write("exit\n");
+		$this->conn->write("terminal no length\n");
+		$this->conn->read($this->shellPattern, $this->conn::READ_REGEX);
+
+		return $output;
+    }
+
+    public function cleanupPatterns()
+    {
+        return [
+            $this->shellPattern,
+            $this->privilegedExecModePattern,
+            $this->configurationModePattern
+        ];
+    }
+}
