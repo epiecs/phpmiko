epiecs/phpmiko

commit 8fdddf0d78d50d7cdc534924f3d256fa5cd79604

Updated the readme with a TOC Updated the readme to be more clear regarding device modes
Thanks @murrant!
Cleaned up the code to now properly disconnect at the end of a command run
Removed duplicate uneccesary disconnects from the cisco_ios handler

src/ConnectionHandler.php
@@ -127,6 +127,8 @@ class ConnectionHandler
 
         echo ($this->verbose ? $this->deviceConnection->conn->getLog() : false);
 
+        $this->disconnect();
+
         return $output;
 	}
 
@@ -139,6 +141,8 @@ class ConnectionHandler
 
         echo ($this->verbose ? $this->deviceConnection->conn->getLog() : false);
 
+        $this->disconnect();
+
         return $output;
 	}
 
@@ -150,6 +154,8 @@ class ConnectionHandler
         $output = $this->raw ? $output : $this->cleanOutput($output, $commands);
 
         echo ($this->verbose ? $this->deviceConnection->conn->getLog() : false);
+
+        $this->disconnect();
 
         return $output;
 	}
@@ -245,6 +251,7 @@ class ConnectionHandler
 
 	public function disconnect() : void
 	{
+        $this->deviceConnection->conn->disconnect();
 		unset($this->deviceConnection);
 	}
 }

src/Devices/Cisco_ios.php
@@ -95,9 +95,6 @@ class Cisco_ios implements DeviceInterface
 			$output[$command] = $this->conn->read($this->privilegedExecModePattern, $this->conn::READ_REGEX);
 		}
 
-		// Exit the cli
-		$this->conn->disconnect();
-
 		return $output;
     }
 
@@ -139,9 +136,6 @@ class Cisco_ios implements DeviceInterface
 			// Read the data and add it to the output
 			$output[$command] = $this->conn->read($this->configurationModePattern, $this->conn::READ_REGEX);
 		}
-
-        // Exit the cli
-		$this->conn->disconnect();
 
 		return $output;
     }

src/Devices/Junos.php
@@ -27,7 +27,7 @@ class Junos implements DeviceInterface
     private $configurationModePattern = '/.*@.*#\s/m';
 
 
-    // TODO: Rekening houden met [yes,no] vragen in het pattern. Best werken met een | OR pipe
+    // TODO: Take into account [yes,no] questions in the pattern. Maybe use a OR match in the pattern?
     // request system power-off at 1907292300 all-members
     // warning: This command will halt all the members.
     // If planning to halt only one member use the member option

commit 90ec5d9ad9b65e25af40399228be1c0f58784d87

Updated the readme with a quickstart example fixed issue #3 where commit 8fdddf0 broke sequentual commands

src/ConnectionHandler.php
@@ -132,8 +132,6 @@ class ConnectionHandler
 
         echo ($this->verbose ? $this->deviceConnection->conn->getLog() : false);
 
-        $this->disconnect();
-
         return $output;
 	}
 
@@ -146,8 +144,6 @@ class ConnectionHandler
 
         echo ($this->verbose ? $this->deviceConnection->conn->getLog() : false);
 
-        $this->disconnect();
-
         return $output;
 	}
 
@@ -159,8 +155,6 @@ class ConnectionHandler
         $output = $this->raw ? $output : $this->cleanOutput($output, $commands);
 
         echo ($this->verbose ? $this->deviceConnection->conn->getLog() : false);
-
-        $this->disconnect();
 
         return $output;
 	}
@@ -259,4 +253,9 @@ class ConnectionHandler
         $this->deviceConnection->conn->disconnect();
 		unset($this->deviceConnection);
 	}
+
+    public function __destruct()
+    {
+        $this->disconnect();
+    }
 }